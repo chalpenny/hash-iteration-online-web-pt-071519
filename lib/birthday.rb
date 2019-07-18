1910fd23ed9468a885c75f3e57ecdd385cfc09f7
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-07-18 04:41:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,8 @@
 
 def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
-      if age =< 12
+      if age < 13
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
+  end
 end
